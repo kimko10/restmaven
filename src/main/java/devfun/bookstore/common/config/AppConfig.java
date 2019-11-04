ed12b002e244966857d25f44a795503a757e8852
kimko10 <56960857+kimko10@users.noreply.github.com>
2019-11-05 00:31:25 +0900

    설정정보 초기 작성중

--- src/main/java/devfun/bookstore/common/config/AppConfig.java
+++ src/main/java/devfun/bookstore/common/config/AppConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
 
 	/*
-	 * 데이터베이스 접속, 트랜잭션 관리, DAO와 Service를 정의 부분	 * 
+	 * 데이터베이스 접속, 트랜잭션 관리, DAO와 Service를 정의 부분 
 	 */
 	
 	
