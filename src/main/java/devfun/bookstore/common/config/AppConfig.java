a27f1cead0373490277e0cf976d569fbe8a74c25
kimko10 <56960857+kimko10@users.noreply.github.com>
2019-11-05 00:25:02 +0900

    app / servelt config 초기설정

--- src/main/java/devfun/bookstore/common/config/AppConfig.java
+++ src/main/java/devfun/bookstore/common/config/AppConfig.java
@@ -1,5 +1,13 @@
 package devfun.bookstore.common.config;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppConfig {
 
+	/*
+	 * 데이터베이스 접속, 트랜잭션 관리, DAO와 Service를 정의 부분	 * 
+	 */
+	
+	
 }
--- src/main/java/devfun/bookstore/rest/config/RestConfig.java
+++ src/main/java/devfun/bookstore/rest/config/RestConfig.java
@@ -1,5 +1,12 @@
 package devfun.bookstore.rest.config;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RestConfig {
+	
+	/*
+	 * 웹 어플리케이션 설정 클래스
+	 */
 
 }
