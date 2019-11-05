be7562e5b2d0a89789ca76789a45a5933b28d371
kimko10 <56960857+kimko10@users.noreply.github.com>
2019-11-05 18:50:14 +0900

    Book 객체 생성

--- src/main/java/devfun/bookstore/common/domain/Book.java
+++ src/main/java/devfun/bookstore/common/domain/Book.java
@@ -1,5 +1,49 @@
 package devfun.bookstore.common.domain;
 
+import java.util.Date;
+
 public class Book {
 
+	private int id;
+	private String title;
+	private String creator;
+	private String type;
+	private Date date;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getCreator() {
+		return creator;
+	}
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", creator=" + creator + ", type=" + type + ", date=" + date
+				+ "]";
+	}
+	
 }
