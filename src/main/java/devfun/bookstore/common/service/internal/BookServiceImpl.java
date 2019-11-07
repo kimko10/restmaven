74d6deeff178e4d7fe87f8c9989ee34623856d8e
kimko10 <56960857+kimko10@users.noreply.github.com>
2019-11-07 22:10:15 +0900

    ServiceImpl 주석 추가

--- src/main/java/devfun/bookstore/common/service/internal/BookServiceImpl.java
+++ src/main/java/devfun/bookstore/common/service/internal/BookServiceImpl.java
@@ -17,28 +17,46 @@ public class BookServiceImpl implements BookService {
 	
 	@Autowired BookMapper bookMapper;
 
+	/**
+	 * 도서 정보 목록 조회
+	 */
 	@Override
 	public List<Book> getBooks() {
 		return bookMapper.select();
 	}
 
+	/**
+	 * 도서 정보 조회
+	 */
 	@Override
 	public Book getBook(int id) {
 		return bookMapper.selectByPrimaryKey(id);
 	}
 
+	/**
+	 * 도서 정보 등록
+	 * 트랜잭션 처리 추가
+	 */
 	@Override
 	@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
 	public int createBook(Book book) {
 		return bookMapper.insert(book);
 	}
 
+	/**
+	 * 도서 정보 수정
+	 * 트랜잭션 처리 추가
+	 */
 	@Override
 	@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
 	public int updateBook(Book book) {
 		return bookMapper.updateByPrimaryKey(book);
 	}
 
+	/**
+	 * 도서 정보 삭제
+	 * 트랜잭션 처리 추가
+	 */
 	@Override
 	@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
 	public int deleteBook(int id) {
